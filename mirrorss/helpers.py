870e1210bb92cef2efd38d5a72c22e3185d8463a
maximiliancw <wunderkind.serie-0f@icloud.com>
2023-07-22 18:59:16 +0200

    Update helpers.py

--- mirrorss/helpers.py
+++ mirrorss/helpers.py
@@ -7,6 +7,6 @@ def is_valid_url(s: str) -> bool:
         return False
     return True
 
-def less_than_1h_ago(then: float) -> bool:
+def less_than_x_hours_ago(then: float, hours: int) -> bool:
     now = datetime.now().timestamp()
-    return (now - then) < 3600
+    return (now - then) < hours * 3600
